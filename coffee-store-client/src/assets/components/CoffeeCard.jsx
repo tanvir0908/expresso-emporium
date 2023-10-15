7bf6435fd6b63afda03d6c6b7b2f4e92c4bef9aa
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-10-15 22:08:25 +0600

    coffee delete operation added

--- coffee-store-client/src/assets/components/CoffeeCard.jsx
+++ coffee-store-client/src/assets/components/CoffeeCard.jsx
@@ -1,20 +1,51 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import Swal from "sweetalert2";
+
 export default function CoffeeCard({ coffee }) {
-  const { name, chef, supplier, taste, category, details, photo } = coffee;
+  const handleDelete = (id) => {
+    console.log(id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your coffee has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+
+  const { _id, name, chef, supplier, taste, category, details, photo } = coffee;
   return (
-    <div className="flex justify-between">
-      <div className="w-3/4">
+    <div className="grid grid-cols-6">
+      <div className="col-span-2">
         <img src={photo} alt="" />
       </div>
-      <div>
-        <h2>{name}</h2>
-        <p>{details}</p>
+      <div className="col-span-3 ml-2">
+        <h2 className="text-2xl font-medium">Name: {name}</h2>
+        <p className="text-lg font-medium text-gray-500">Price: {details}</p>
       </div>
       <div className="btn-group btn-group-vertical">
-        <button className="btn btn-active">Details</button>
+        <button className="btn">Details</button>
         <button className="btn">Edit</button>
-        <button className="btn">Delete</button>
+        <button className="btn bg-red-500" onClick={() => handleDelete(_id)}>
+          Delete
+        </button>
       </div>
     </div>
   );
--- coffee-store-client/src/assets/components/Home.jsx
+++ coffee-store-client/src/assets/components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   const coffees = useLoaderData();
 
   return (
-    <div className="mx-auto">
+    <div className="mx-96">
       <h3 className="text-4xl my-10 font-bold text-center">This is home</h3>
       <div className="grid grid-cols-2 gap-10">
         {coffees.map((coffee) => (
